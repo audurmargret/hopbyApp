06a4ab95d4640a22aed31f0467d99f63de4bd96e
Jóhann Sigurður Jóhannsson <43797947+JohannSJohannsson@users.noreply.github.com>
2021-03-23 12:48:03 +0000

    Bætti við basic maps í MapsActivity
    mapsActivity sem á að opna kort sem bendir á Reykjavík. Takkarnir í sessionOverview virka ekki og ég er búinn að reyna að laga það í alltof langan tíma skil ekkert

---
+++ app/src/main/java/is/hi/hbv601g/hopby/MapsActivity.java
@@ -0,0 +1,45 @@
+package is.hi.hbv601g.hopby;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Reykjavík and move the camera
+        LatLng reykjavik = new LatLng(64.1466, -21.9426);
+        mMap.addMarker(new MarkerOptions().position(reykjavik).title("Marker in Reykjavík"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(reykjavik, 15f));
+    }
+}
