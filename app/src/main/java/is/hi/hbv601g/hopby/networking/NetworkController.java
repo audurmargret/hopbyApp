c2505a2db78638fa865156b6d22250f4c16cf914
audurmargret <42755272+audurmargret@users.noreply.github.com>
2021-03-25 17:04:24 +0000

    jeij birtir sesion
    en ekki dags og tima

--- app/src/main/java/is/hi/hbv601g/hopby/networking/NetworkController.java
+++ app/src/main/java/is/hi/hbv601g/hopby/networking/NetworkController.java
@@ -10,6 +10,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
+import com.google.gson.JsonSerializer;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 
