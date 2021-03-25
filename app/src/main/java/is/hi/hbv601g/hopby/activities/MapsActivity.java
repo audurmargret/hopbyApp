ed5217806f7d68730313470935e816889722cfd7
Jóhann Sigurður Jóhannsson <43797947+JohannSJohannsson@users.noreply.github.com>
2021-03-25 16:32:40 +0000

    þarf að breyta öllu bíðið aðeins

--- app/src/main/java/is/hi/hbv601g/hopby/activities/MapsActivity.java
+++ app/src/main/java/is/hi/hbv601g/hopby/activities/MapsActivity.java
@@ -1,20 +1,41 @@
 package is.hi.hbv601g.hopby.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import is.hi.hbv601g.hopby.R;
-
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import is.hi.hbv601g.hopby.R;
+
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Button mapButton;
+    private ArrayList<String[]> markers = new ArrayList <String[]>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +61,101 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // Add a marker in Reykjavík and move the camera
         LatLng reykjavik = new LatLng(64.1466, -21.9426);
-        mMap.addMarker(new MarkerOptions().position(reykjavik).title("Marker in Reykjavík"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(reykjavik, 15f));
+
+        markers.add(new String[]{"Reykjavík","Ganga"});
+        markers.add(new String[]{"Laugalækjarskóli","Körfubolti"});
+        markers.add(new String[]{"Álftamýri","Fótbolti"});
+        markers.add(new String[]{"Laugardalsvöllur","Fótbolti"});
+        markers.add(new String[]{"Akureyri","Ganga"});
+        markers.add(new String[]{"Fagradalsfjall", "Ganga"});
+        addMarkers(markers);
+
+        mapButton = (Button) findViewById(R.id.button);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MapsActivity.this, SessionOverviewActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    private void addMarkers(ArrayList markers) {
+
+        int lenMarkers = markers.size();
+        String [] locspo;
+
+        for(int i = 0; i < lenMarkers; i++) {
+            locspo = (String[]) markers.get(i);
+            String loc = locspo[0];
+            String sport = locspo[1];
+            switch (sport) {
+                case "Körfubolti":
+                    mMap.addMarker((new MarkerOptions().position(getLocationFromAddress(getApplicationContext(), loc)).title(sport + " " + loc).icon(BitmapFromVector(getApplicationContext(), R.drawable.ic_baseline_sports_basketball_24))));
+                    break;
+                case "Fótbolti":
+                    mMap.addMarker((new MarkerOptions().position(getLocationFromAddress(getApplicationContext(), loc)).title(sport + " " + loc).icon(BitmapFromVector(getApplicationContext(), R.drawable.ic_baseline_sports_soccer_24))));
+                    break;
+                default:
+                    mMap.addMarker((new MarkerOptions().position(getLocationFromAddress(getApplicationContext(), loc)).title(sport + " " + loc).icon(BitmapFromVector(getApplicationContext(), R.drawable.ic_baseline_directions_walk_24))));
+            }
+        }
+    }
+
+    private BitmapDescriptor BitmapFromVector(Context context, int vectorResId) {
+        // below line is use to generate a drawable.
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+
+        // below line is use to set bounds to our vector drawable.
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+
+        // below line is use to create a bitmap for our
+        // drawable which we have added.
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+
+        // below line is use to add bitmap in our canvas.
+        Canvas canvas = new Canvas(bitmap);
+
+        // below line is use to draw our
+        // vector drawable in canvas.
+        vectorDrawable.draw(canvas);
+
+        // after generating our bitmap we are returning our bitmap.
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+    private LatLng getLocationFromAddress(Context context, String inputtedAddress) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng resLatLng = null;
+
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(inputtedAddress, 5);
+            if (address == null) {
+                return null;
+            }
+
+            if (address.size() == 0) {
+                return null;
+            }
+
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            resLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+            Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        return resLatLng;
     }
 }
