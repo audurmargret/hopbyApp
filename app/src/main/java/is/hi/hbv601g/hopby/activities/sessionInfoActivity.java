5049c8a4817a45441c048a7754dabc7e8092c296
katlarun9 <43798256+katlarun9@users.noreply.github.com>
2021-03-30 17:45:20 +0000

    meira session info

--- app/src/main/java/is/hi/hbv601g/hopby/activities/sessionInfoActivity.java
+++ app/src/main/java/is/hi/hbv601g/hopby/activities/sessionInfoActivity.java
@@ -3,14 +3,23 @@ package is.hi.hbv601g.hopby.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
+import java.util.ArrayList;
+
+import is.hi.hbv601g.hopby.InfoModel;
 import is.hi.hbv601g.hopby.R;
+import is.hi.hbv601g.hopby.entities.Session;
 
 public class sessionInfoActivity extends AppCompatActivity {
 
+    GridView grid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_info);
+        grid = findViewById(R.id.info_grid);
+        ArrayList<InfoModel> sessionArrayList = new ArrayList<InfoModel>();
+        sessionArrayList.add(new InfoModel("blaah", "Title"));
     }
 }
